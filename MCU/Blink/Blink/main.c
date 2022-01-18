f1af74f9351de4062132371673e0c1c5138d8318
cdgarcia420 <97985905+cdgarcia420@users.noreply.github.com>
2022-01-18 15:35:17 -0500

    Add files via upload

---
+++ MCU/Blink/Blink/main.c
@@ -0,0 +1,24 @@
+/*
+ * Blink.c
+ *
+ * Created: 1/18/2022 2:29:36 PM
+ * Author : Cameron Garcia
+ */ 
+
+#include <avr/io.h>
+#include <avr/power.h>
+
+
+int main(void)
+{
+	clock_prescale_set(clock_div_1);   // 16 MHz
+	DDRB = 0xFF; // Make B7 to B0 output pins
+
+	while (1)
+	{
+		PORTB = ~PORTB; // Complement every bit
+		for (volatile int32_t n = 0; n < 47057; n++){}
+	}
+}
+
+
